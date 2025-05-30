f077718311515c7917569535d0938c44d6878aa1
Upscope CI <ci@upscope.io>
2025-05-30 21:11:01 +0000

    Release v2025.5.9

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "UpscopeIO",
             url: "https://github.com/upscopeio/cobrowsing-ios/releases/download/v2025.5.9/UpscopeIO-2025.5.9.xcframework.zip",
-            checksum: "6b055059a9d8f2d0d68500b8ec7ead9b2cbf9a1b71cb1daeca8b50271a19406b"
+            checksum: "e0eaa2c0249fa675a1dc6eb5486d35d0b4c8af8a3185976a39d66c49c3e9307f"
         )
     ]
 )
