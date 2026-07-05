0d703a7004e872810ddbef824f1889f76b493f2c
Upscope CI <ci@upscope.io>
2026-07-05 23:56:04 +0000

    Release v2026.7.2

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UpscopeSDK",
-            url: "https://github.com/upscopeio/cobrowsing-ios/releases/download/v2026.7.1/UpscopeSDK-2026.7.1.xcframework.zip",
-            checksum: "60555968e2299ce9fefd1d4e75749538b915569a95329cc42a6ad86e65508d11"
+            url: "https://github.com/upscopeio/cobrowsing-ios/releases/download/v2026.7.2/UpscopeSDK-2026.7.2.xcframework.zip",
+            checksum: "e874cb4d7e76ec21b0afeb78e503d11d62cf8c5a9c6c413d05932204b753f489"
         ),
         .binaryTarget(
             name: "UpscopeBroadcastExtension",
-            url: "https://github.com/upscopeio/cobrowsing-ios/releases/download/v2026.7.1/UpscopeBroadcastExtension-2026.7.1.xcframework.zip",
-            checksum: "771dc7c0ed90919a90f22f1b24af731ee0410d3dbaa48a891937396043f6943f"
+            url: "https://github.com/upscopeio/cobrowsing-ios/releases/download/v2026.7.2/UpscopeBroadcastExtension-2026.7.2.xcframework.zip",
+            checksum: "4928cdb69493e9bb3903a47ab17ce155cdd1e13de8d633418e744191c214b9f7"
         )
     ]
 )
